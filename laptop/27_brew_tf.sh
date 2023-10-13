ef49be55615e1d9317129304beee8ee9eee524e3
David Raminiak <davidraminiak@Davids-MacBook-Pro.local>
2023-10-13 13:33:06 -0500

    new

---
+++ laptop/27_brew_tf.sh
@@ -0,0 +1,12 @@
+# Terraform
+## https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli
+
+brew tap hashicorp/tap
+brew install hashicorp/tap/terraform
+
+brew update
+brew upgrade hashicorp/tap/terraform
+
+terraform -help
+terraform -install-autocomplete
+
